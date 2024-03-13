fd4d14fa6af2c6ff02c7b47bbfdb2fabc94c2955
Kelson Ameida <kelson.victr@gmail.com>
2024-03-13 20:04:12 -0300

    Aula 02 - exercicio1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RenderizandoComFuncoes from './components/RenderizandoComFuncoes'
+
+const App = () => {
+  return (
+    <div>
+      <RenderizandoComFuncoes />
+    </div>
+  )
+}
+
+export default App
